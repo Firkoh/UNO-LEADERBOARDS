c09aa9fceccd7183cfd699d0db3d4288a1bf58a9
FIRKOH <150876334+Firkoh@users.noreply.github.com>
2026-07-18 19:25:40 +0900

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -35,7 +35,7 @@ export default function Login() {
       return;
     }
 
-    if (data.user?.email === "admin@gmail.com") {
+    if (data.user?.email === "admin@gmail.com" && "vio@gmail.com") {
       navigate("/admin");
     } else {
       navigate("/");
